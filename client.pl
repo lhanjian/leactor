cb74c75da40179299b8bde1f09478881d191c8f4
lhanjian <lhjay1@gmail.com>
2014-10-04 21:22:15 +0800

    modified:   client.pl 	new file:   test.bin

--- client.pl
+++ client.pl
@@ -7,46 +7,42 @@ my $sock = IO::Socket::INET->new(PeerAddr=>'localhost',
                               PeerPort=>'12344',
                               Proto   =>'tcp',) or die("Failed !");
 #$sock->write("test", 10, 0):
-$sock->autoflush(1);
+#$sock->autoflush(1);
 print "connected to the server\n";
 my $wait = <>;
 print "wait end\n";
     
 
-my $rcv = 
-threads->create(sub {
-    threads->detach();
+my $rcv = threads->create(sub {
+        threads->detach();
 
-while (1) {
-    print "recv S\n";
-    my $msg;
-    $sock->recv($msg, 32);#or die "recvF\n";
-    print "recv O\n";
-    print ">".$msg."\0";
-    print "recv P\n";
-}
-
+        while (1) {
+            print "recv S\n";
+            my $msg;
+            defined $sock->recv($msg, 32) or die "recvF:error<$!>\n";
+            print "recv O\n";
+            print ">".$msg."\0";
+            print "recv P\n";
+        }
 });
 
-while (1) {
+if (1) {
+    my $snd = 
+    threads->create(sub {
+            threads->detach();
 
-my $snd = 
-threads->create(sub {
-    threads->detach();
+            while (1) {
+                print "send S\n";
+                my $data = "dATA\n";
+                defined $sock->send($data) or die "sendF: $!";
+                print "send O\n";
+                sleep 5;
+            }
 
-while (1) {
-    print "send S\n";
-    my $data = "dATA";
-    $sock->send($data) or die "sendF";
-    print "send O\n";
-    sleep 5;
+        });
 }
 
-});
-
-}
-
-sleep 15;
+sleep 15000;
 print "sleep over\n";
 
 $sock->close();
