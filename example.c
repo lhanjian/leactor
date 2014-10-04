655497ec21bad3e358214e236cd927e73b1c32ff
lhanjian <lhjay1@gmail.com>
2014-10-04 16:02:27 +0800

    Revert "	new file:   client.pl"
    This reverts commit 5d3c4cb3fae40c12a3ca0a6e47cea6554976573d.
    
    	modified:   lea_heap.c

--- example.c
+++ example.c
@@ -12,6 +12,7 @@ int incoming(int, void *);
 int play_back(int, void *);
 
 base_t *base;
+int new_in_fd;
 
 int main(void)
 {
@@ -69,14 +70,13 @@ int incoming(int test, void *arg)
     socklen_t addr_len = sizeof(sa);
     int my_sock = *(int *)arg;
 
-    int *new_in_fd = malloc(sizeof(int));
-    *new_in_fd = accept(my_sock, (struct sockaddr *)&sa, &addr_len);
-    if (*new_in_fd == -1) {
+    new_in_fd = accept(my_sock, (struct sockaddr *)&sa, &addr_len);
+    if (new_in_fd == -1) {
         perror("acpt");
         return -1;;
     }
 
-    lt_io_add(base, *new_in_fd, LV_FDRD|LV_CONN, play_back, new_in_fd, INF);
+    lt_io_add(base, new_in_fd, LV_FDRD|LV_CONN, play_back, &new_in_fd, INF);
 
     return 0;
 }
