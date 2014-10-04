80debea37da5f4130778449bd30c61525f9106b5
lhanjian <lhjay1@gmail.com>
2014-10-04 16:12:23 +0800

    new file:   compile.sh 	modified:   event_lea.h

---
+++ compile.sh
@@ -0,0 +1,3 @@
+gcc event_lea.c -c event_lea.c -Wall -g -m32 -std=gnu99
+gcc lea_heap.c -c event_lea.c -Wall -g -m32 -std=gnu99
+gcc example.c -c event_lea.c -Wall -g -m32 -std=gnu99
--- event_lea.h
+++ event_lea.h
@@ -125,6 +125,6 @@ res_t base_dispatch(base_t *base_dispatch
 /*
 res_t base_free(base_t *base_rlve);
 */
-#include "lea_heap.h"
+//#include "lea_heap.h"
 #endif
 
